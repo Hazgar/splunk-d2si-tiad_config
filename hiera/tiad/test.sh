0d0ff16f3db848f022bcaaf3d0718c7562269531
Stephane Lapie <slapie@splunk.com>
2015-03-19 00:00:48 +0100

    Add timestamp to fake test logs

--- hiera/tiad/test.sh
+++ hiera/tiad/test.sh
@@ -2,19 +2,24 @@
 THIS=${BASH_SOURCE[0]}
 THIS_PATH=$(dirname ${BASH_SOURCE[0]})
 
+function fmt_date ()
+{
+  date +"%Y-%m-%d %H:%M:%S"
+}
+
 mkdir $THIS_PATH/tmp
 for file in $THIS_PATH/*.yaml; do
- echo "Processing $file"
+ echo "$(fmt_date) Processing $file"
  ln -sfn $PWD/$file $THIS_PATH/tmp/student.yaml
  result=$(facter --external-dir=$THIS_PATH/tmp tiad_student)
  if [ "x$result" == "x" ] ; then
-  echo "Error with '$file'"
+  echo "$(fmt_date) Error with '$file'"
   rm -rf $THIS_PATH/tmp
   exit 2
  fi
  cat << EOF | ruby
 test=$result
-printf("user=\"%s\" email=\"%s\" message=\"%s\"\n", test['name'], test['email'], test['message'])
+printf("%s user=\"%s\" email=\"%s\" message=\"%s\"\n", Time.now.strftime("%Y-%m-%d %H:%M:%S"), test['name'], test['email'], test['message'])
 EOF
 done
 rm -rf $THIS_PATH/tmp
